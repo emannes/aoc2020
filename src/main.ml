dc5b88539650917a2c67d8deec345f4fdb01a952
Eric Mannes <emannes@gmail.com>
2020-12-15 04:52:55 -0500

    13B

--- src/main.ml
+++ src/main.ml
@@ -3,6 +3,7 @@ open! Async
 
 let print_int n = printf "%d\n" n
 let parse_as_input s = String.split_lines s |> List.map ~f:String.strip
+let positive_mod n k = ((n mod k) + k) mod k
 
 module Subpart = struct
   type t =
@@ -847,8 +848,6 @@ module Problem11 : S = struct
 end
 
 module Problem12 : S = struct
-  let positive_mod n k = ((n mod k) + k) mod k
-
   module Direction = struct
     type t =
       [ `N
@@ -990,6 +989,25 @@ module Problem13 : S = struct
     let of_string s : t = if String.( = ) s "x" then None else Some (Int.of_string s)
   end
 
+  let multiplicative_inverse k ~mod_ =
+    if k mod mod_ = 0
+    then None
+    else (
+      let rec search_from i i_times_k =
+        if i_times_k = 1 then i else search_from (i + 1) ((i_times_k + k) mod mod_)
+      in
+      Some (search_from 1 (k mod mod_)))
+  ;;
+
+  let%expect_test "multiplicative_inverse" =
+    List.iter [ 5, 17; 0, 17; 17, 17 ] ~f:(fun (k, mod_) ->
+        printf !"%{sexp: int option}\n" (multiplicative_inverse k ~mod_));
+    [%expect {|
+      (7)
+      ()
+      () |}]
+  ;;
+
   let solve subpart file_contents =
     let earliest_departure_time, buses =
       match file_contents with
@@ -1008,7 +1026,27 @@ module Problem13 : S = struct
         |> Map.min_elt_exn
       in
       print_int (frequency * wait_time)
-    | B -> failwith "not implemented"
+    | B ->
+      let residues =
+        List.filter_mapi buses ~f:(fun k -> function
+          | Some n -> Some (n, positive_mod (-k) n)
+          | None -> None)
+      in
+      let product = List.map ~f:fst residues |> List.reduce_exn ~f:( * ) in
+      let answer =
+        List.sum
+          (module Int)
+          residues
+          ~f:(fun (n, k) ->
+            let product_of_other_moduli = product / n in
+            let addthis =
+              k
+              * product_of_other_moduli
+              * Option.value_exn (multiplicative_inverse product_of_other_moduli ~mod_:n)
+            in
+            addthis)
+      in
+      print_int (answer mod product)
   ;;
 
   let%expect_test _ =
@@ -1016,11 +1054,9 @@ module Problem13 : S = struct
   7,13,x,x,59,x,31,19|} |> parse_as_input in
     solve A input;
     let%bind () = [%expect {| 295 |}] in
-    return ()
+    solve B input;
+    [%expect {| 1068781 |}]
   ;;
-
-  (*     solve B input;
-    [%expect {| 286 |}] *)
 end
 
 module Not_implemented : S = struct
