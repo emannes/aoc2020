81ea3169eb040c8f6d056b490ab6a585e10fd37a
Eric Mannes <emannes@gmail.com>
2020-12-20 22:20:02 -0500

    common.mli, why not

---
+++ src/common.mli
@@ -0,0 +1,21 @@
+open! Core
+
+val print_int : int -> unit
+val parse_as_input : string -> string list
+val positive_mod : int -> int -> int
+val split_into_groups : string list -> string list list
+
+module Subpart : sig
+  type t =
+    | A
+    | B
+  [@@deriving sexp]
+end
+
+module Pair : sig
+  type t = int * int [@@deriving sexp, compare]
+
+  include Comparable.S with type t := t
+
+  val ( + ) : t -> t -> t
+end
