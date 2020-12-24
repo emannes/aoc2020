94b10cb4bd367b2151c912c1adb55b8c64098d99
Eric Mannes <emannes@gmail.com>
2020-12-24 14:04:21 -0500

    23 int-based solution also too intensive for B

--- src/problem23.ml
+++ src/problem23.ml
@@ -2,95 +2,8 @@ open! Core
 open Common
 
 module State = struct
-  type t =
-    { cup_by_position : int Float.Map.t
-    ; position_by_cup : float Int.Map.t
-    ; current_cup : int
-    }
-  [@@deriving sexp]
-
-  let of_list cups =
-    let num_cups = List.length cups in
-    let cups_n_positions =
-      List.mapi cups ~f:(fun i cup -> Float.of_int (i + num_cups), cup)
-    in
-    { cup_by_position = Float.Map.of_alist_exn cups_n_positions
-    ; position_by_cup =
-        Int.Map.of_alist_exn (List.map cups_n_positions ~f:(fun (a, b) -> b, a))
-    ; current_cup = List.hd_exn cups
-    }
-  ;;
-
-  let closest_key_wrapping map how key =
-    match Map.closest_key map how key with
-    | Some (next_key, value) -> next_key, value
-    | None ->
-      (match how with
-      | `Greater_or_equal_to | `Greater_than -> Map.min_elt_exn map
-      | `Less_or_equal_to | `Less_than -> Map.max_elt_exn map)
-  ;;
-
-  let remove_all map keys = List.fold keys ~init:map ~f:Map.remove
-
-  let step { cup_by_position; position_by_cup; current_cup } =
-    let current_cup_position = Map.find_exn position_by_cup current_cup in
-    let next_cup_after position =
-      closest_key_wrapping cup_by_position `Greater_than position
-    in
-    let next_position, next_cup = next_cup_after current_cup_position in
-    let next'_position, next'_cup = next_cup_after next_position in
-    let next''_position, next''_cup = next_cup_after next'_position in
-    let cup_by_position =
-      remove_all cup_by_position [ next_position; next'_position; next''_position ]
-    in
-    let position_by_cup =
-      remove_all position_by_cup [ next_cup; next'_cup; next''_cup ]
-    in
-    let _destination_cup, destination_position =
-      closest_key_wrapping position_by_cup `Less_than current_cup
-    in
-    let post_destination_position, _post_destination_cup =
-      closest_key_wrapping cup_by_position `Greater_than destination_position
-    in
-    let new_position, new'_position, new''_position =
-      let post_destination_position =
-        if Float.( < ) destination_position post_destination_position
-        then post_destination_position
-        else destination_position +. 1.
-      in
-      let step = (post_destination_position -. destination_position) /. 4. in
-      ( destination_position +. step
-      , destination_position +. (2. *. step)
-      , destination_position +. (3. *. step) )
-    in
-    let cup_by_position =
-      Map.add_exn cup_by_position ~key:new_position ~data:next_cup
-      |> Map.add_exn ~key:new'_position ~data:next'_cup
-      |> Map.add_exn ~key:new''_position ~data:next''_cup
-    in
-    let position_by_cup =
-      Map.add_exn position_by_cup ~data:new_position ~key:next_cup
-      |> Map.add_exn ~data:new'_position ~key:next'_cup
-      |> Map.add_exn ~data:new''_position ~key:next''_cup
-    in
-    let _new_current_cup_position, new_current_cup =
-      closest_key_wrapping cup_by_position `Greater_than current_cup_position
-    in
-    print_s
-      [%message
-        (current_cup : int)
-          (current_cup_position : float)
-          (next_cup : int)
-          (next_position : float)
-          (next'_cup : int)
-          (next'_position : float)
-          (next''_cup : int)
-          (next''_position : float)
-          (destination_position : float)
-          (post_destination_position : float)
-          (new_current_cup : int)];
-    { cup_by_position; position_by_cup; current_cup = new_current_cup }
-  ;;
+  (* front = current cup, reading clockwise *)
+  type t = int list
 
   let starting_from_1 t =
     let index_of_1 =
@@ -101,21 +14,13 @@ module State = struct
     after_1 @ before_1
   ;;
 
-  let to_A_answer t =
-    Map.data t.cup_by_position
-    |> starting_from_1
-    |> List.map ~f:Int.to_string
-    |> String.concat
-  ;;
+  let to_A_answer t = starting_from_1 t |> List.map ~f:Int.to_string |> String.concat
 
   let to_B_answer t =
-    Map.data t.cup_by_position
-    |> starting_from_1
-    |> (fun cups -> List.take cups 2)
-    |> List.reduce_exn ~f:( * )
+    starting_from_1 t |> (fun cups -> List.take cups 2) |> List.reduce_exn ~f:( * )
   ;;
 
-  (*   let step : t -> t = function
+  let step : t -> t = function
     | current_cup :: next :: next' :: next'' :: rest ->
       let next_three = [ next; next'; next'' ] in
       let destination_cup =
@@ -144,20 +49,15 @@ module State = struct
       endswith_dest @ next_three @ after_dest @ [ current_cup ]
     | _ -> failwith "huh? not enough cups"
   ;;
- *)
+
   let%expect_test _ =
-    let test_input = [ 3; 8; 9; 1; 2; 5; 4; 6; 7 ] |> of_list in
-    print_s [%message (step test_input : t)];
+    let test_input = [ 3; 8; 9; 1; 2; 5; 4; 6; 7 ] in
+    print_s [%message (step test_input : int list)];
     print_s [%message (to_A_answer (Fn.apply_n_times ~n:10 step test_input) : string)];
     print_s [%message (to_A_answer (Fn.apply_n_times ~n:100 step test_input) : string)];
     [%expect
       {|
-      ("step test_input"
-       ((cup_by_position
-         ((9 3) (13 2) (13.25 8) (13.5 9) (13.75 1) (14 5) (15 4) (16 6) (17 7)))
-        (position_by_cup
-         ((1 13.75) (2 13) (3 9) (4 15) (5 14) (6 16) (7 17) (8 13.25) (9 13.5)))
-        (current_cup 2)))
+      ("step test_input" (2 8 9 1 5 4 6 7 3))
       ("to_A_answer (Fn.apply_n_times ~n:10 step test_input)" 92658374)
       ("to_A_answer (Fn.apply_n_times ~n:100 step test_input)" 67384529) |}]
   ;;
@@ -166,19 +66,11 @@ end
 let solve subpart _file_contents =
   let input = [ 9; 6; 3; 2; 7; 5; 4; 8; 1 ] in
   match (subpart : Subpart.t) with
-  | A ->
-    Fn.apply_n_times ~n:100 State.step (State.of_list input)
-    |> State.to_A_answer
-    |> print_endline
+  | A -> Fn.apply_n_times ~n:100 State.step input |> State.to_A_answer |> print_endline
   | B ->
-    (*     let num_cups, num_steps = 1_000_000, 23 in
- *)
-    let num_cups, num_steps = 1_000, 28 in
     let input =
       input
-      @ List.init (num_cups - List.length input) ~f:(fun i -> 1 + i + List.length input)
+      @ List.init (1_000_000 - List.length input) ~f:(fun i -> i + List.length input)
     in
-    Fn.apply_n_times ~n:num_steps State.step (State.of_list input)
-    |> State.to_B_answer
-    |> print_int
+    Fn.apply_n_times ~n:10 State.step input |> State.to_B_answer |> print_int
 ;;
